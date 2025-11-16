7a78272146441940bd6e863a3cb920e60dd29b05
Lucas de Oliveira Mota <lucas.d.mota@ufv.br>
2025-11-16 15:15:05 -0300

    Adiciona exportação em drizzle.config.ts

--- packages/drizzle.config.ts
+++ packages/drizzle.config.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 
-export default defineConfig({
+const config: Config = {
   schema: './drizzle/schema.ts',
   out: './drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
+
+export default config;
