b53d4ded67a6e1ee724ee5859fd1eb5fbc4af287
Lucas de Oliveira Mota <lucas.d.mota@ufv.br>
2025-11-18 16:35:51 -0300

    Adiciona arquivo de configuração Jest

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/apps/server/tsconfig.json',
+      }
+    ],
+  },
+  collectCoverageFrom: [
+    'apps/server/**/*.(t|j)s',
+    '!apps/server/**/*.module.ts',
+    '!apps/server/main.ts',
+    '!apps/server/**/*.dto.ts',
+    '!apps/server/**/*.interface.ts',
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/apps/server'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/apps/server/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
